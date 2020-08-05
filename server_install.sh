f48cd84b7bd632cc52211b152af12273a8692b73
EC2 Default User <ec2-user@ip-172-31-46-80.eu-central-1.compute.internal>
2020-08-05 14:50:10 +0000

    Initial commit.

---
+++ server_install.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+servers=$(jq -r '. | keys | .[]' server_config.json)
+
+inserts=2
+tabs='\ \ \ \ '
+
+version=$(sudo cat /proc/version)
+
+if [ -z "${version##*Red Hat*}" ]; then
+    echo "Red Hat"
+fi
+for i in $servers;do
+    config=$(jq ".\"$i\"" server_config.json)
+    temp_file=$(mktemp)
+
+    sed "s/%%servername%%/ $i/" ./server_template.conf > $temp_file
+
+    keys=$(echo $config | jq -r 'keys | .[]')
+
+    # iterate through config keys
+    # allows except some cases with corrupted or not right config
+    for key in $keys; do
+        case $key in
+            ports)
+                ports=$(echo $config | jq -r ".$key | .[]")
+                listning=""
+                for port in $ports; do
+                    listning="$listning *:$port"
+                done
+
+                if [[ "${ports[*]}" =~ 443 ]]; then
+                    sed -i "$inserts a ${tabs}SSLEngine on" $temp_file
+                    inserts=$((inserts + 1))
+                fi
+
+                sed -i "s/%%ports%%/ $listning/" $temp_file
+                ;;
+            aliases)
+                altnames=($(echo $config | jq -r ".$key | .[]"))
+                if [[ "${#altnames[@]}" > 0 ]]; then
+                    for name in $altnames; do
+                        echo $name
+                    done
+                fi
+                ;;
+            enable)
+                modules=($(echo $config | jq -r ".$key | .[]"))
+                if [[ "${#modules[@]}" > 0 ]]; then
+                    for module in ${modules[@]}; do 
+                        sed -i "$inserts a $tabs$module on" $temp_file
+                    done
+                fi
+                ;;
+            disable)
+                modules=($(echo $config | jq -r ".$key | .[]"))
+                if [[ "${#modules[@]}" > 0 ]]; then
+                    for module in ${modules[@]}; do
+                        sed -i "$inserts a $tabs$module off" $temp_file
+                    done
+                fi
+                ;;
+        *)
+        echo "$key is not supported"
+    esac
+    done
+
+    cat $temp_file
+    rm $temp_file
+done
+   
