1df9a0f5e8d14e8276f61ead62371edf51d4db21
Daniil <silnyagindan@gmail.com>
2020-12-09 21:16:22 +0300

    Checkboxes pointer error fix.

--- jenkins/updateCheckBoxes.sh
+++ jenkins/updateCheckBoxes.sh
@@ -3,14 +3,24 @@
 # $1 - parameters
 # $2 - file
 updateCheckboxes() {
+    local parameter_arr=$1
     echo 'CheckboxParameter:' > $2
-    for parameter in $1; do
+    if [[ "${#parameter_arr}" == 0 ]]; then
+        local parameter_arr="None"
+    fi
+    for parameter in $parameter_arr; do
         echo -e "  - key: $parameter\n    value: $parameter" >> $2
     done
 }
 
-available_files=$(ls ./available/)
-available_services=$(sudo docker service ls --filter name=$docker_service_name --format='{{json .Name}}')
+available_services=""
 
-updateCheckboxes "$available_files" "./jenkins/checkboxes.yaml"
+is_docker_up=$(sudo systemctl is-active docker | grep -q "inactive" && echo "false" || echo "true")
+
+if [[ $is_docker_up = "true" ]]; then
+    available_services=$(sudo docker service ls --filter name=$docker_service_name --format='{{json .Name}}')
+fi
 updateCheckboxes "$available_services" "./jenkins/services.yaml"
+
+available_files=$(ls ./available/)
+updateCheckboxes "$available_files" "./jenkins/checkboxes.yaml"
